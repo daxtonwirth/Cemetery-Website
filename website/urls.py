8633553d2417b7ac18cd90f06d12ca1591237144
dax <daxtonwirth@gmail.com>
2023-02-16 18:39:13 -0800

    added map

--- website/urls.py
+++ website/urls.py
@@ -3,8 +3,9 @@ from . import views
 from .views import SearchResultsView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.map_view, name="home"),
     path('add', views.add, name="add"),
     path('veterans', views.veterans, name="veterans"),
     path("search/", SearchResultsView.as_view(), name="search"),
+    path('map/', views.map_view, name='map'),
 ]
--- website/views.py
+++ website/views.py
@@ -6,9 +6,9 @@ from django.contrib import messages
 from django.db.models import Q 
 
 
-def home(request):
-    all_people = Grave.objects.all
-    return render(request, 'home.html', {'all':all_people})
+#def home(request):
+#    all_people = Grave.objects.all
+#    return render(request, 'home.html', {'all':all_people})
 
 def veterans(request):
     all_people = Grave.objects.all
@@ -46,3 +46,24 @@ class SearchResultsView(ListView):
             term = query
             object_list = Grave.objects.filter(Q(FirstName__icontains=term) | Q(LastName__icontains=term))
         return object_list
+    
+    
+    
+    
+    
+import googlemaps
+from django.shortcuts import render
+
+def map_view(request):
+    # Create a client with the API key
+    gmaps = googlemaps.Client(key='AIzaSyBQZ0MOCrRpEhYwjgsXisPvJ98Ajn0fFAw')
+
+    # Make a request to the Google Maps API to retrieve data
+    data = gmaps.geocode('4023 Ririe Hwy, Rigby, ID 83442')
+
+    # Extract the latitude and longitude from the data
+    lat = data[0]['geometry']['location']['lat']
+    lng = data[0]['geometry']['location']['lng']
+
+    # Render the template with the latitude and longitude
+    return render(request, 'home.html', {'lat': lat, 'lng': lng})
